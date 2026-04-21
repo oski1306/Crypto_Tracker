e96783897330f878397f729281d4388347a3ca16
oski1306 <oski1306@o2.pl>
2026-04-21 18:23:17 +0200

    Last update 21.04

--- main.js
+++ main.js
@@ -3,13 +3,95 @@ let socket;
 const modal = document.getElementById("modal");
 const addBtn = document.getElementById("addCoinBtn");
 const closeModal = document.getElementById("closeModal");
+const settingsModal = document.getElementById("settingsModal");
+const closeSettings = document.getElementById("closeSettings");
 const coinList = document.getElementById("coinList");
 const emptyState = document.getElementById("empty-state");
+const applyBtn = document.querySelector(".apply-btn");
+const confirmModal = document.getElementById("confirmModal");
+const closeConfirm = document.getElementById("closeConfirm");
+const deleteModal = document.getElementById("deleteModal");
+const closeDelete = document.getElementById("closeDelete");
+const deleteBtn = document.querySelector(".delete-btn");
 
 const coinSelectModal = document.getElementById("coinSelectModal");
 const amountInput = document.querySelector(".modal-content input");
 
 let portfolio = [];
+let selectedIndex = null;
+let pendingUpdate = null;
+
+deleteBtn.addEventListener("click", () => {
+    const item = portfolio[selectedIndex];
+
+    const symbol = item.coin === "btcusdt" ? "BTC" : "ETH";
+
+    document.getElementById("deleteCoin").textContent = symbol;
+    document.getElementById("deleteAmount").textContent = item.amount;
+
+    settingsModal.classList.add("hidden");
+    deleteModal.classList.remove("hidden");
+});
+
+document.getElementById("deleteYes").addEventListener("click", () => {
+    portfolio.splice(selectedIndex, 1);
+
+    deleteModal.classList.add("hidden");
+
+    updateUI();
+});
+
+document.getElementById("deleteNo").addEventListener("click", () => {
+    deleteModal.classList.add("hidden");
+    settingsModal.classList.remove("hidden");
+});
+
+closeDelete.addEventListener("click", () => {
+    deleteModal.classList.add("hidden");
+});
+
+deleteModal.addEventListener("click", (e) => {
+    if (e.target === deleteModal) {
+        deleteModal.classList.add("hidden");
+    }
+});
+
+closeConfirm.addEventListener("click", () => {
+    confirmModal.classList.add("hidden");
+});
+
+confirmModal.addEventListener("click", (e) => {
+    if (e.target === confirmModal) {
+        confirmModal.classList.add("hidden");
+    }
+});
+
+applyBtn.addEventListener("click", () => {
+    const item = portfolio[selectedIndex];
+
+    const newAmount = parseFloat(document.querySelector("#settingsModal input").value);
+
+    if (!newAmount || newAmount <= 0) return;
+
+    pendingUpdate = {
+        index: selectedIndex,
+        oldAmount: item.amount,
+        newAmount: newAmount,
+        oldCurrency: "USD",
+        newCurrency: "USD"
+    };
+
+    document.getElementById("oldAmount").textContent = pendingUpdate.oldAmount;
+    document.getElementById("newAmount").textContent = pendingUpdate.newAmount;
+
+    document.getElementById("oldCurrency").textContent = pendingUpdate.oldCurrency;
+    document.getElementById("newCurrency").textContent = pendingUpdate.newCurrency;
+
+    settingsModal.classList.add("hidden");
+    confirmModal.classList.remove("hidden");
+})
+
+
 
 addBtn.addEventListener("click", () => {
     modal.classList.remove("hidden");
@@ -17,6 +99,28 @@ addBtn.addEventListener("click", () => {
 
 closeModal.addEventListener("click", () => {
     modal.classList.add("hidden");
+});
+
+coinList.addEventListener("click", (e) => {
+    if (e.target.classList.contains("settings-btn")) {
+        selectedIndex = e.target.dataset.index;
+
+        const item = portfolio[selectedIndex];
+
+        document.querySelector("#settingsModal input").value = item.amount;
+
+        settingsModal.classList.remove("hidden");
+    }
+});
+
+closeSettings.addEventListener("click", () => {
+    settingsModal.classList.add("hidden");
+});
+
+settingsModal.addEventListener("click", (e) => {
+    if (e.target === settingsModal) {
+        settingsModal.classList.add("hidden");
+    }
 });
 
 document.querySelector(".add-btn").addEventListener("click", () => {
@@ -38,6 +142,21 @@ document.querySelector(".add-btn").addEventListener("click", () => {
     connectSocket(coin);
 });
 
+document.getElementById("confirmYes").addEventListener("click", () => {
+    const {index, newAmount} = pendingUpdate;
+
+    portfolio[index].amount = newAmount;
+
+    confirmModal.classList.add("hidden");
+
+    updateUI();
+});
+
+document.getElementById("confirmNo").addEventListener("click", () => {
+    confirmModal.classList.add("hidden");
+    settingsModal.classList.remove("hidden");
+});
+
 function updateUI() {
     if (portfolio.length === 0) {
         emptyState.style.display = "flex";
@@ -56,7 +175,7 @@ function updateUI() {
         <div class="coin-item">
         <div class="coin-header">
         <div class="coin-title">${symbol}</div>
-        ⚙️
+        <span class="settings-btn" data-index="${index}">⚙️</span>
         </div>
         
         <div class="coin-section">
